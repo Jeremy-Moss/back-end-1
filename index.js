b027063f6252f312d6cd966b526d35ba06121360
Jeremy-Moss <103158555+Jeremy-Moss@users.noreply.github.com>
2022-05-21 15:43:49 -0400

    minor change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/api/users", (req, res) => {
 
 app.get("/weather/:temperature", (req, res) => {
     const { temperature } = req.params;
-    const phrase = `<h3It was ${temperature} today</h3>`;
+    const phrase = `<h3>It was ${temperature} today</h3>`;
     res.status(200).send(phrase);
 });
 app.listen(4000, () => console.log("Server running on port 4000"));
